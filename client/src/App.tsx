44241149a4069a2ecfff2a3049a0237a4c7c35a6
elisha <elishajameel2007@gmail.com>
2025-09-14 19:18:33 +0500

    fix the big of browser router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <div className="pt-20">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/shop" element={<h1>Shop Page</h1>} />
+            <Route path="/about" element={<h1>About Page</h1>} />
+            <Route path="/contact" element={<h1>Contact Page</h1>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
