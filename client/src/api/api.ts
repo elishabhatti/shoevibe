38166f8ba5f39215e39414525a38cb59fcf09a60
Noman <pxycknomdictator@outlook.com>
2025-09-16 02:18:07 +0500

    fixed: fixed formatting

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL || "",
-  headers: { "Content-Type": "application/json" },
+    baseURL: import.meta.env.VITE_SERVER_URL || "",
+    headers: { "Content-Type": "application/json" },
 });
 
 export const renderProducts = async () => {
-  const response = await axiosInstance.get("/products");
-  return response.data;
+    const response = await axiosInstance.get("/products");
+    return response.data;
 };
