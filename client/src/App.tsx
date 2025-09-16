b5e3550cb087c7d8642bd3231c864697541af670
elisha <elishajameel2007@gmail.com>
2025-09-16 14:34:49 +0500

    created pages and added routes for that

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -11,9 +14,9 @@ const App = () => {
         <div className="pt-20">
           <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/shop" element={<h1>Shop Page</h1>} />
-            <Route path="/about" element={<h1>About Page</h1>} />
-            <Route path="/contact" element={<h1>Contact Page</h1>} />
+            <Route path="/shop" element={<Shop/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
           </Routes>
         </div>
         <Footer />
